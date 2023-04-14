b57730092c54cb44202b3542919afdf7628ed59e
Antigen-1 <zhanghao@antigen.top>
2023-04-14 12:19:57 +0800

    update the contract

--- huffman.rkt
+++ huffman.rkt
@@ -29,6 +29,7 @@
 
 (require racket/contract)
 
+(define leaf/c (list/c any/c byte?))
 (define node/c (list/c any/c set? any/c any/c))
 
 (define (insert-node o l (r null))
@@ -79,7 +80,7 @@
                                  min max))))
 
 (define/contract (ordered-list->huffman-tree l)
-  (-> (non-empty-listof (or/c node/c node-is-leaf?)) node/c); at least one node is required to call this function and the value returned by this function must not be a leaf
+  (-> (non-empty-listof (or/c node/c leaf/c)) node/c); at least one element is required to call this function and the value returned by this function must not be a leaf
   (cond ((null? (cdr l)) (car l))
         (else (ordered-list->huffman-tree (insert-node (merge-two-nodes (car l) (cadr l)) (cddr l))))))
 
