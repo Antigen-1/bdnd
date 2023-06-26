067aaabd0eb42e2cce462300c3674f2e5284fb04
Antigen-1 <zhanghao@antigen.top>
2023-06-26 20:34:12 +0800

    fix info.rkt

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "bdnd")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "typed-racket-lib"))
 (define scribblings '(("scribblings/bdnd.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
